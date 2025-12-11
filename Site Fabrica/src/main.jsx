4ee15554c0d21a4cb03ef65b243369f565f46e4e
Fábrica de Testadores <fabrica.testadores@uece.br>
2025-12-11 13:08:30 -0300

    Use Vite BASE_URL for Router basename

--- Site Fabrica/src/main.jsx
+++ Site Fabrica/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+const basename = import.meta.env.BASE_URL || '/'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Site-Fabrica">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
